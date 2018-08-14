36cb99458ae739bfa5b81c337179f0333f1d0fec
johnstonstu <stu.w.johnston@gmail.com>
2018-08-14 09:37:46 -0700

    added test witn num in the string

--- __tests__/string-compression.spec.js
+++ __tests__/string-compression.spec.js
@@ -3,8 +3,26 @@ const stringCompression = require("../lib/stringCompression.js");
 describe("stringCompresion", () => {
   describe("when a string is entered", () => {
     it("should return each letter followed immediately by a number representing the number of occourances in a row", () => {
-      const result = frogJumps("aaabbb");
+      const result = stringCompression("aaabbb");
       expect(result).toEqual("a3b3");
+    });
+  });
+  describe("when an incorrect data type is entered", () => {
+    it("should return error", () => {
+      const result = stringCompression(15);
+      expect(result).toEqual("error");
+    });
+  });
+  describe("when a parameter is missing", () => {
+    it("should return error", () => {
+      const result = stringCompression();
+      expect(result).toEqual("error");
+    });
+  });
+  describe("when a number is entered in the string", () => {
+    it("should return error", () => {
+      const result = stringCompression("aa11bb");
+      expect(result).toEqual("a2b2");
     });
   });
 });
