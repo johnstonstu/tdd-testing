8cdd863e0e39996bb2b4b61d615605b16ab7e669
johnstonstu <stu.w.johnston@gmail.com>
2018-08-14 15:13:50 -0700

    refactor min num arr

--- __tests__/min-num-array.spec.js
+++ __tests__/min-num-array.spec.js
@@ -22,7 +22,7 @@ describe("stringCompresion", () => {
   describe("when all elements are the mixed elemtnts in the array", () => {
     it("should return error", () => {
       const result = minNumArr([1, "two", 3]);
-      expect(result).toEqual(2);
+      expect(result).toEqual(undefined);
     });
   });
   describe("when all elements are the negetive in the array", () => {
@@ -34,6 +34,22 @@ describe("stringCompresion", () => {
   describe("given an array of n integers", () => {
     it("returns the minimal positive integer (greater than 0) that does not occur in the array", () => {
       const result = minNumArr([1, 3, 6, 4, 1, 2]);
+      expect(result).toEqual(5);
+    });
+  });
+  describe("given an array of large n integers", () => {
+    it("returns the minimal positive integer (greater than 0) that does not occur in the array", () => {
+      const result = minNumArr([
+        1,
+        2,
+        3,
+        4,
+        11213123,
+        3123123,
+        612312312,
+        434654334,
+        23453453
+      ]);
       expect(result).toEqual(5);
     });
   });
